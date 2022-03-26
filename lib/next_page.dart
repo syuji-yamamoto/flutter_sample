e070d59c69fa3cad098e449b974244be69165cf4
syuji-yamamoto <shuji.yamamoto1105@gmail.com>
2022-03-26 22:18:29 +0900

    first commit

---
+++ lib/next_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next Page'),
+      ),
+      body: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
